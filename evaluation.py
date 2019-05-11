1ad15101814c603039ccbf35f17c91a6b30e45ca
许越 <xuyue@xuyuedeMacBook-Pro.local>
2019-05-11 22:29:36 +0800

    modified:   evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -14,14 +14,14 @@ class Metrics(object):
 
     def __init__(self, SR=None, GT=None):
         if SR is not None and GT is not None:
-            print(SR.shape)
-            self.acc = get_accuracy(SR,GT)
-            self.SE  = get_sensitivity(SR,GT)
-            self.SP  = get_specificity(SR,GT)
-            self.PC  = get_precision(SR,GT)
-            self.F1  = get_F1(SR,GT)
-            self.JS  = get_JS(SR,GT)
-            self.DC  = get_DC(SR,GT)
+            for i in range(SP.shape[0]):
+                self.acc = get_accuracy(   SR[i,...], GT[i,...])
+                self.SE  = get_sensitivity(SR[i,...], GT[i,...])
+                self.SP  = get_specificity(SR[i,...], GT[i,...])
+                self.PC  = get_precision(  SR[i,...], GT[i,...])
+                self.F1  = get_F1(         SR[i,...], GT[i,...])
+                self.JS  = get_JS(         SR[i,...], GT[i,...])
+                self.DC  = get_DC(         SR[i,...], GT[i,...])
 
 
     def add(self, metrics):
