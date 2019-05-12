b812a687192d26b84ae7ee95d213121b239e42e3
许越 <xuyue@xuyuedeMacBook-Pro.local>
2019-05-12 22:49:05 +0800

    modified:   solver.py

--- solver.py
+++ solver.py
@@ -155,12 +155,9 @@ class Solver(object):
 
 			if i%10==0:
 				SR = SR.cpu()
-				GT = GT.cpu()
 				sr0 = torchvision.transforms.ToPILImage()(SR[0, ...]*255)
-				gt0 = torchvision.transforms.ToPILImage()(GT[0, ...]*255)
 				os.makedirs("/content/drive/image_log/", exist_ok=True)
 				sr0.save("/content/drive/image_log/{}_pred.jpg".format(i))
-				gt0.save("/content/drive/image_log/{}_gt.jpg".format(i))
 			
 
 		metrics.div(length)
