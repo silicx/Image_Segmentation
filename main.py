9c48a08261353a16969eb5dfc5108e3893e42f94
许越 <xuyue@xuyuedeMacBook-Pro.local>
2019-06-03 21:59:43 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def test_3D(config, data_dir, save_dir):
                     if config.name == 'axis1':
                         data = data.transpose((1,0,2,3))
                         print(data.shape)
-                        dset[:,i,:,:] = 
+                        dset[:,i,:,:] = data
                     elif config.name == 'axis2':
                         data = data.transpose((1,2,0,3))
                         dset[:,:,i,:] = data
