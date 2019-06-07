58b326d24df41db4ff87008f556357fed20dcff3
许越 <xuyue@xuyuedeMacBook-Pro.local>
2019-06-07 14:01:28 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def inference_3D(config, data_dir, save_dir):
             
             for i in range(n_image):
                 if (i+1)%32==0:
-                    logging.info("[{}/{}]".format(i+1, n_image))
+                    logging.info("[{}/{}]".format(i+1, data.shape[0]))
                 
                 img = data[i,...]
                 img = Image.fromarray(img)
